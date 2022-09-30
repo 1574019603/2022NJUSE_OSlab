8a7a180465502cd47173234380c25ac57b88c9ed
wangxiao <1574019603@qq.com>
2022-09-30 21:30:10 +0800

    提交

--- lab1/big_cal.asm
+++ lab1/big_cal.asm
@@ -1,6 +1,7 @@
 ;已定义数据区
 section .data
 tip1: db "please input the operator X and Y:",0Ah
+tip2: db "invalid input",0Ah
 anotherLine: db 0Ah
 
 
@@ -12,6 +13,7 @@ operator1: resb 255
 len1: resb 255
 operator2: resb 255
 len2: resb 255
+caozuofu: resb 255;操作符
 
 
 
@@ -22,8 +24,45 @@ section .text
     global main ;程序入口
 
 main:
+  .getin:
     mov eax,tip1
-    
+    call printStr ;输出提示语句
+
+    mov ecx,input;ecx=input
+    call getInput ;提取输入
+
+    ;如果是q，跳转到结束模块
+    cmp byte[input],'q'
+    jz endin
+
+    ;否则进入后续处理
+    ;获取操作符,存入caozuofu
+    call getOperatorAndCaozuofu
+
+
+  .endin
+;结束程序
+    mov eax, 1 
+    mov ebx, 0 
+    int 80h 
+
+
+
+getOperatorAndCaozuofu:
+;此函数用于获取操作符,输出：caozuofu
+    pushad
+    mov byte[caozuofu],0
+    mov eax,input
+    mov ebx,eax
+    mov ecx,operator1
+    mov edx,operator2
+    mov esi,len1
+    mov edi,len2
+    mov byte[esi],0
+    mov byte[edi],0
+
+  .find
+
 
 
 
@@ -64,3 +103,13 @@ getStrLen:
     ret
 
 
+
+getInput:
+;获得键盘输入
+    pushad
+    mov edx,255
+    mov eax,3
+    mov ebx,0
+    int 80h
+    popad
+    ret
